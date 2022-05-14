d0a038d7a0760c75ebb079c6f21b11295da38075
manisrivi <manisrivi1995@mail.com>
2022-05-14 20:30:46 +0530

    price-card

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
 
-function App() {
+export default function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className=" container-fluid">
+      <div className="row justify-content-center">
+        <div className="col-lg-3 col-md-5 col-sm-8 p-4 content ">
+        <Msg 
+      head1="FREE" 
+      head2="$0"
+      c1="✔ Single User"
+      c2="✔ 5GB Storage"
+      c3="✔ Unlimited Public Projects"
+      c4="✔ Community Access"
+      c5="✘ Unlimited Private Projects"
+      c6="✘ Dedicated Phone Support"
+      c7="✘ Free Subdomain"
+      c8="✘ Monthly Status Reports"
+      />
+      </div>
+        <div className="col-lg-3 col-md-5 col-sm-8 p-4 content">
+        <Msg 
+              head1="PLUS" 
+              head2="$9"
+              c1="✔ 5 Users"
+              c2="✔ 50GB Storage"
+              c3="✔ Unlimited Public Projects"
+              c4="✔ Community Access"
+              c5="✔ Unlimited Private Projects"
+              c6="✔ Dedicated Phone Support"
+              c7="✔ Free Subdomain"
+              c8="✘ Monthly Status Reports"
+        />
+        </div>
+        <div className="col-lg-3 col-md-5 col-sm-8 p-4 content">
+        <Msg 
+              head1="PRO" 
+              head2="$49"
+              c1="✔ Unlimited Users"
+              c2="✔ 150GB Storage"
+              c3="✔ Unlimited Public Projects"
+              c4="✔ Community Access"
+              c5="✔ Unlimited Private Projects"
+              c6="✔ Dedicated Phone Support"
+              c7="✔ Free Subdomain"
+              c8="✔ Monthly Status Reports"
+        />
+        </div>
+      </div>
     </div>
+
   );
 }
 
-export default App;
+// Components
+function Msg({head1, head2, c1,c2,c3,c4,c5,c6,c7,c8}){
+  return (
+    <div>
+    <div className="text-center">  
+    <p className="fw-bold opacity-50">{head1}</p>
+    <h4 className="display-6 fw-bold">{head2}<span>/month</span></h4>
+    <hr/>
+    </div>
+    <p>{c1}</p>
+    <p>{c2}</p>
+    <p>{c3}</p>
+    <p>{c4}</p>
+    <p className="c5">{c5}</p>
+    <p className="c6">{c6}</p>
+    <p className="c7">{c7}</p>
+    <p className="c8">{c8}</p>
+   <div className="text-center"><button className="btn btn-primary w-100 rounded-pill">BUTTON</button></div>
+    </div>
+    );
+}
+
+
+
